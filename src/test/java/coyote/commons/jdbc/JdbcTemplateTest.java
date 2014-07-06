e3c75e0cfdce5b9b7c9b1785968f2074c675e510
Steve Cote <sdcote@gmail.com>
2014-07-05 20:06:58 -0400

    improved testing coverage

--- src/test/java/coyote/commons/jdbc/JdbcTemplateTest.java
+++ src/test/java/coyote/commons/jdbc/JdbcTemplateTest.java
@@ -2,9 +2,14 @@ package coyote.commons.jdbc;
 
 //import static org.junit.Assert.*;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.List;
 
@@ -34,6 +39,66 @@ public class JdbcTemplateTest {
     ds.setURL( "jdbc:h2:mem:test;MODE=Oracle;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE" );
     ds.setUser( "sa" );
     ds.setPassword( "" );
+
+    SimpleDateFormat dateformat = new SimpleDateFormat( "yyyy/MM/dd" );
+
+    // use regular JDBC to populate a small database as an illustration of the
+    // efficiencies gained with the JDBC helper classes being tested
+
+    Connection conn = null;
+    Statement stmt = null;
+    PreparedStatement pstmt;
+
+    try {
+      conn = ds.getConnection( "sa", "" );
+      conn.setAutoCommit( true );
+
+      stmt = conn.createStatement();
+
+      String sql = "CREATE TABLE EMPLOYEE (emp_no INTEGER not NULL, birth_date DATE, first_name VARCHAR(32), last_name VARCHAR(32), gender VARCHAR(32), hire_date DATE, PRIMARY KEY ( emp_no ))";
+
+      stmt.executeUpdate( sql );
+
+      String query = "insert into EMPLOYEE(emp_no, birth_date, first_name,last_name,gender,hire_date) values(?, ?, ?, ?, ?, ?)";
+      pstmt = conn.prepareStatement( query );
+
+      // / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
+      // Create employees in the test database
+      pstmt.setInt( 1, 1001 );
+      pstmt.setDate( 2, new Date( dateformat.parse( "1974/06/22" ).getTime() ) );
+      pstmt.setString( 3, "Robert" );
+      pstmt.setString( 4, "White" );
+      pstmt.setString( 5, "Male" );
+      pstmt.setDate( 6, new Date( dateformat.parse( "2004/01/04" ).getTime() ) );
+      pstmt.executeUpdate();
+
+      pstmt.setInt( 1, 1002 );
+      pstmt.setDate( 2, new Date( dateformat.parse( "1974/10/06" ).getTime() ) );
+      pstmt.setString( 3, "Alice" );
+      pstmt.setString( 4, "McKensy" );
+      pstmt.setString( 5, "Female" );
+      pstmt.setDate( 6, new Date( dateformat.parse( "2003/06/12" ).getTime() ) );
+      pstmt.executeUpdate();
+
+      // / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
+
+    } catch ( SQLException se ) {
+      se.printStackTrace();
+    } catch ( Exception e ) {
+      e.printStackTrace();
+    }
+    finally {
+      //finally block used to close resources
+      try {
+        if ( stmt != null )
+          conn.close();
+      } catch ( SQLException se ) {}// do nothing
+      try {
+        if ( conn != null )
+          conn.close();
+      } catch ( SQLException se ) {}// do nothing
+    }
+
   }
 
 
@@ -60,15 +125,17 @@ public class JdbcTemplateTest {
 
 
 
-  @SuppressWarnings("unchecked")
+  @Test
   public void anotherTest() {
 
-    // This creates the thing which converts rows in the result set into objects
-    DataMapper employeeDataMapper = new DataMapper<Employee>() {
+    ResultMapper<Employee> employeeDataMapper = new ResultMapper<Employee>() {
       @Override
       public Employee map( ExtendedResultSet rs ) throws SQLException {
-        Integer empNo = rs.getNullableInt( "emp_no" ); // Get an integer from the result set.
-                                                       // It returns null when it is SQL NULL.
+
+        //System.out.println( "Pos:" + rs.getRow() );
+
+        // Get an integer from the result set.
+        Integer empNo = rs.getNullableInt( "emp_no" );
         Date birthDate = rs.getDate( "birth_date" );
         String firstName = rs.getString( "first_name" );
         String lastName = rs.getString( "last_name" );
@@ -80,17 +147,26 @@ public class JdbcTemplateTest {
 
     JdbcTemplate template = new JdbcTemplate( ds );
 
-    List<Employee> tenEmployees = template.query( "SELECT * FROM employees LIMIT ?, ?;", Arrays.<Object> asList( 0, 10 ), employeeDataMapper );
+    List<Employee> emps = template.query( "SELECT * FROM EMPLOYEE LIMIT ?, ?;", Arrays.<Object> asList( 0, 5 ), employeeDataMapper );
+    // System.out.println( "Found " + emps.size() + " records" );
+    assertTrue( emps.size() == 2 );
 
-    //Employee bob = template.updateOne( "UPDATE employees SET firstName=? WHERE emp_no=?;", Arrays.<Object> asList( "Bob", 1001 ), employeeDataMapper );
+
+    emps = template.query( "SELECT * FROM EMPLOYEE WHERE emp_no= ?;", Arrays.<Object> asList( 1001 ), employeeDataMapper );
+    // System.out.println( "Found " + emps.size() + " records" );
+    // System.out.println( emps.get( 0 ) );
+    assertTrue( emps.size() == 1 );
 
   }
 
 
 
 
-  public void yetAnotherTest() {
-
+  @Test
+  public void updateTest() {
+    JdbcTemplate template = new JdbcTemplate( ds );
+    int count = template.update( "UPDATE EMPLOYEE SET first_name=? WHERE emp_no=?;", Arrays.<Object> asList( "Bob", 1001 ) );
+    assertTrue( count == 1 );
   }
 
 }
@@ -102,9 +178,39 @@ public class JdbcTemplateTest {
 //
 //
 class Employee {
+  Integer empNo;
+  Date birthDate;
+  String firstName;
+  String lastName;
+  Gender gender;
+  Date hireDate;
+
+
+
 
   public Employee( Integer empNo, Date birthDate, String firstName, String lastName, Gender gender, Date hireDate ) {
-    // TODO Auto-generated constructor stub
+    this.empNo = empNo;
+    this.birthDate = birthDate;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.gender = gender;
+    this.hireDate = hireDate;
+  }
+
+
+
+
+  @Override
+  public String toString() {
+    StringBuffer buffer = new StringBuffer( "Employee:" );
+    buffer.append( empNo );
+    buffer.append( " Name:" );
+    buffer.append( firstName );
+    buffer.append( " " );
+    buffer.append( lastName );
+    buffer.append( " Hired:" );
+    buffer.append( hireDate );
+    return buffer.toString();
   }
 }
 
@@ -128,6 +234,7 @@ class Gender {
 
 
   static Gender toGender( String text ) {
+    // convert M to Male, F to Female, etc.
     return new Gender( "Unknown" );
   }
 }
