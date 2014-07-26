c0c5fd8b72075a43f88656e23f049c7218e5e830
Steve Cote <sdcote@gmail.com>
2014-07-26 15:39:53 -0400

    Start of an internal logger

---
+++ src/main/java/coyote/commons/Log.java
@@ -0,0 +1,121 @@
+package coyote.commons;
+
+import java.io.PrintStream;
+
+
+/**
+ * Internal logger.
+ */
+public final class Log {
+
+  public static enum Level {
+    TRACE, DEBUG, INFO, WARN, ERROR
+  }
+
+  public static final boolean TRACE = Boolean.getBoolean( Log.class.getName() + ".trace" );
+
+  public static final boolean DEBUG = TRACE || Boolean.getBoolean( Log.class.getName() + ".debug" );
+
+  private static PrintStream output = System.out;
+
+
+
+
+  public static void debug( final Object... messages ) {
+    if ( TRACE || DEBUG ) {
+      log( Level.DEBUG, messages );
+    }
+  }
+
+
+
+
+  public static void trace( final Object... messages ) {
+    if ( TRACE ) {
+      log( Level.TRACE, messages );
+    }
+  }
+
+
+
+
+  public static void warn( final Object... messages ) {
+    log( Level.WARN, messages );
+  }
+
+
+
+
+  public static void error( final Object... messages ) {
+    log( Level.ERROR, messages );
+  }
+
+
+
+
+  public static PrintStream getOutput() {
+    return output;
+  }
+
+
+
+
+  /**
+   */
+  public static void info( final Object... messages ) {
+    log( Level.INFO, messages );
+  }
+
+
+
+
+  static void log( final Level level, final Object... messages ) {
+    synchronized( output ) {
+      output.format( "[%s] ", level );
+
+      for ( int i = 0; i < messages.length; i++ ) {
+        if ( ( ( i + 1 ) == messages.length ) && ( messages[i] instanceof Throwable ) ) {
+          output.println();
+          ( (Throwable)messages[i] ).printStackTrace( output );
+        } else {
+          render( output, messages[i] );
+        }
+      }
+
+      output.println();
+      output.flush();
+    }
+  }
+
+
+
+
+  /**
+   * Helper to support rendering messages.
+   */
+  static void render( final PrintStream out, final Object message ) {
+    if ( message.getClass().isArray() ) {
+      final Object[] array = (Object[])message;
+
+      out.print( "[" );
+      for ( int i = 0; i < array.length; i++ ) {
+        out.print( array[i] );
+        if ( ( i + 1 ) < array.length ) {
+          out.print( "," );
+        }
+      }
+      out.print( "]" );
+    } else {
+      out.print( message );
+    }
+  }
+
+
+
+
+  public static void setOutput( final PrintStream out ) {
+    Assert.notNull( out );
+    output = out;
+  }
+
+}
