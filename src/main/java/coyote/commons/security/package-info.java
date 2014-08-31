db0039f71444b7382eebb267cb7e8125f23be6b7
Steve Cote <sdcote@gmail.com>
2014-08-31 14:08:29 -0400

    Adding encryption utilities

---
+++ src/main/java/coyote/commons/security/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * A collection of security functions common to many different products.
+ * 
+ * <p><strong>CAVEAT:</strong> The encryption algorithms in this package are 
+ * NOT intended for production and now warranties are given as to their 
+ * applicability for a given purpose. These methods are fine for development 
+ * purposes, but the reader is strongly advised to carefully consider their 
+ * choice in using encryption libraries. These are fine for proof of concept 
+ * systems but are not intended for critical systems.</p>
+ */
+package coyote.commons.security;
