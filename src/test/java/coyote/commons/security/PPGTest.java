8362e1e1daec142165d754b2bc9241efd888ec33
Steve Cote <sdcote@gmail.com>
2014-09-02 07:55:03 -0400

    Added pronounceable pass phrase test cases

---
+++ src/test/java/coyote/commons/security/PPGTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2014 Stephan D. Cote' - All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the 
+ * terms of the MIT License which accompanies this distribution, and is 
+ * available at http://creativecommons.org/licenses/MIT/
+ *
+ * Contributors:
+ *   Stephan D. Cote 
+ *      - Initial concept and initial implementation
+ */
+package coyote.commons.security;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+/**
+ * 
+ */
+public class PPGTest {
+
+  /**
+   * Test method for {@link coyote.commons.security.PPG#generate(int, int, int)}.
+   */
+  @Test
+  public void testGenerate() {
+    String phrase = PPG.generate( 3, 3, 5 );
+    String[] tokens = phrase.split( " " );
+    assertTrue( tokens.length == 3 );
+    phrase = PPG.generate( 4, 3, 5 );
+    tokens = phrase.split( " " );
+    assertTrue( tokens.length == 4 );
+    phrase = PPG.generate( 5, 3, 5 );
+    tokens = phrase.split( " " );
+    assertTrue( tokens.length == 5 );
+    phrase = PPG.generate( 6, 3, 5 );
+    tokens = phrase.split( " " );
+    assertTrue( tokens.length == 6 );
+  }
+
+
+
+
+  /**
+   * Test method for {@link coyote.commons.security.PPG#generateToken(int)}.
+   */
+  @Test
+  public void testGenerateToken() {
+    String token = PPG.generateToken( 3 );
+    assertTrue( token.length() == 3 );
+    token = PPG.generateToken( 4 );
+    assertTrue( token.length() == 4 );
+    token = PPG.generateToken( 5 );
+    assertTrue( token.length() == 5 );
+    token = PPG.generateToken( 6 );
+    assertTrue( token.length() == 6 );
+  }
+
+}
