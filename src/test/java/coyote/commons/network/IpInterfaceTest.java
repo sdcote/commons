416537116653ff6ab09f5ed9208ab07c01942652
Steve Cote <sdcote@gmail.com>
2014-09-23 14:43:10 -0400

    Added simple test.

---
+++ src/test/java/coyote/commons/network/IpInterfaceTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2014 Stephan D. Cote' - All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the 
+ * terms of the MIT License which accompanies this distribution, and is 
+ * available at http://creativecommons.org/licenses/MIT/
+ *
+ * Contributors:
+ *   Stephan D. Cote 
+ *      - Initial concept and initial implementation
+ */
+package coyote.commons.network;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+public class IpInterfaceTest {
+
+	/**
+	 * Test method for {@link coyote.commons.network.IpInterface#getPrimary()}.
+	 */
+	@Test
+	public void testGetPrimary() {
+		IpInterface intrfc = IpInterface.getPrimary();
+		assertNotNull(intrfc);
+		System.out.println(intrfc);
+	}
+
+}
