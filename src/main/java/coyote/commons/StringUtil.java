32d23ed97fcccc85b6d0eaf480ad94385022a39c
Steve Cote <sdcote@gmail.com>
2015-04-04 08:51:05 -0400

    Added simple string escaping method.

--- src/main/java/coyote/commons/StringUtil.java
+++ src/main/java/coyote/commons/StringUtil.java
@@ -1411,6 +1411,79 @@ public final class StringUtil {
 
 
   /**
+   * Escape quotes, \, /, \r, \n, \b, \f, \t and other control characters (U+0000 through U+001F).
+   * 
+   * @param s The string to process
+   * 
+   * @return a whole new string with the appropriate characters escaped
+   */
+  public static String escape( String s ) {
+    if ( s == null )
+      return null;
+    StringBuffer sb = new StringBuffer();
+    escape( s, sb );
+    return sb.toString();
+  }
+
+
+
+
+  /**
+   * Escape the characters in the given string and place the results at the end 
+   * of the given string buffer.
+   * 
+   * @param s the string to process - Must not be null.
+   * @param sb the buffer to fill
+   */
+  static void escape( String s, StringBuffer sb ) {
+    final int len = s.length();
+    for ( int i = 0; i < len; i++ ) {
+      char ch = s.charAt( i );
+      switch ( ch ) {
+        case '"':
+          sb.append( "\\\"" );
+          break;
+        case '\\':
+          sb.append( "\\\\" );
+          break;
+        case '\b':
+          sb.append( "\\b" );
+          break;
+        case '\f':
+          sb.append( "\\f" );
+          break;
+        case '\n':
+          sb.append( "\\n" );
+          break;
+        case '\r':
+          sb.append( "\\r" );
+          break;
+        case '\t':
+          sb.append( "\\t" );
+          break;
+        case '/':
+          sb.append( "\\/" );
+          break;
+        default:
+          //Reference: http://www.unicode.org/versions/Unicode5.1.0/
+          if ( ( ch >= '\u0000' && ch <= '\u001F' ) || ( ch >= '\u007F' && ch <= '\u009F' ) || ( ch >= '\u2000' && ch <= '\u20FF' ) ) {
+            String ss = Integer.toHexString( ch );
+            sb.append( "\\u" );
+            for ( int k = 0; k < 4 - ss.length(); k++ ) {
+              sb.append( '0' );
+            }
+            sb.append( ss.toUpperCase() );
+          } else {
+            sb.append( ch );
+          }
+      }
+    }//for
+  }
+
+
+
+
+  /**
    * Return the string after the last occurrence of the given character in the
    * given string.
    * 
