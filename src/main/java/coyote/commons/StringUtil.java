f3587d4e349c3122beef6c2e98813f2bdcfc59af
Steve Cote <sdcote@gmail.com>
2015-05-06 17:36:24 -0400

    Promoted LATIN-1 encoding test to ready

--- src/main/java/coyote/commons/StringUtil.java
+++ src/main/java/coyote/commons/StringUtil.java
@@ -87,7 +87,22 @@ public final class StringUtil {
   public static final String UTF_8 = "UTF-8";
 
 
+  /** Field ISO8859_1 */
+  public static String ISO8859_1;
+  static {
+    final String iso = System.getProperty( "ISO_8859_1" );
+    if ( iso != null ) {
+      StringUtil.ISO8859_1 = iso;
+    } else {
+      try {
+        new String( new byte[] { (byte)20 }, "ISO-8859-1" );
 
+        StringUtil.ISO8859_1 = "ISO-8859-1";
+      } catch ( final java.io.UnsupportedEncodingException e ) {
+        StringUtil.ISO8859_1 = "ISO8859_1";
+      }
+    }
+  }
 
   /**
    * Private constructor because everything is static
@@ -829,22 +844,7 @@ public final class StringUtil {
   /* * * * * * * * * These methods are still being tested. * * * * * * * * * * */
   /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-  /** Field ISO8859_1 */
-  public static String ISO8859_1;
-  static {
-    final String iso = System.getProperty( "ISO_8859_1" );
-    if ( iso != null ) {
-      StringUtil.ISO8859_1 = iso;
-    } else {
-      try {
-        new String( new byte[] { (byte)20 }, "ISO-8859-1" );
-
-        StringUtil.ISO8859_1 = "ISO-8859-1";
-      } catch ( final java.io.UnsupportedEncodingException e ) {
-        StringUtil.ISO8859_1 = "ISO8859_1";
-      }
-    }
-  }
+ 
 
   private static final char[] LOWERCASES = { '\000', '\001', '\002', '\003', '\004', '\005', '\006', '\007', '\010', '\011', '\012', '\013', '\014', '\015', '\016', '\017', '\020', '\021', '\022', '\023', '\024', '\025', '\026', '\027', '\030', '\031', '\032', '\033', '\034', '\035', '\036', '\037', '\040', '\041', '\042', '\043', '\044', '\045', '\046', '\047', '\050', '\051', '\052', '\053', '\054', '\055', '\056', '\057', '\060', '\061', '\062', '\063', '\064', '\065', '\066', '\067', '\070', '\071', '\072', '\073', '\074', '\075', '\076', '\077', '\100', '\141', '\142', '\143', '\144', '\145', '\146', '\147', '\150', '\151', '\152', '\153', '\154', '\155', '\156', '\157', '\160', '\161', '\162', '\163', '\164', '\165', '\166', '\167', '\170', '\171', '\172', '\133', '\134', '\135', '\136', '\137', '\140', '\141', '\142', '\143', '\144', '\145', '\146', '\147', '\150', '\151', '\152', '\153', '\154', '\155', '\156', '\157', '\160', '\161', '\162', '\163', '\164', '\165', '\166', '\167', '\170', '\171',
       '\172', '\173', '\174', '\175', '\176', '\177' };
