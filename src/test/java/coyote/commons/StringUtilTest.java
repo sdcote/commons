ca2736e9f6ce2f8a66d6ea873da8134bc43438d2
Steve Cote <sdcote@gmail.com>
2015-07-25 16:50:11 -0400

    fixedLength method for fixed length file creation

--- src/test/java/coyote/commons/StringUtilTest.java
+++ src/test/java/coyote/commons/StringUtilTest.java
@@ -258,4 +258,67 @@ public class StringUtilTest {
     assertEquals( "-foo", StringUtil.stripLeadingHyphens( "---foo" ) );
     assertNull( StringUtil.stripLeadingHyphens( null ) );
   }
+  
+  @Test
+  public void fixedLengthTest() {
+    String text = "Coyote";
+    String field = null;
+    int LEFT = 0;
+    int CENTER = 1;
+    int RIGHT = 2;
+
+    // Alignment Tests = = = = = =
+    field = StringUtil.fixedLength( text, 10, LEFT, '*' );
+    //System.out.println( field );
+    assertTrue( field.length() == 10 );
+    assertEquals( field, "Coyote****" );
+
+    field = StringUtil.fixedLength( text, 10, CENTER, '*' );
+    assertTrue( field.length() == 10 );
+    assertEquals( field, "**Coyote**" );
+
+    field = StringUtil.fixedLength( text, 10, RIGHT, '*' );
+    assertTrue( field.length() == 10 );
+    assertEquals( field, "****Coyote" );
+
+    // Size Match Tests = = = = = 
+    field = StringUtil.fixedLength( text, 6, LEFT, '*' );
+    assertTrue( field.length() == 6 );
+    assertEquals( field, "Coyote" );
+
+    field = StringUtil.fixedLength( text, 6, CENTER, '*' );
+    assertTrue( field.length() == 6 );
+    assertEquals( field, "Coyote" );
+
+    field = StringUtil.fixedLength( text, 6, RIGHT, '*' );
+    assertTrue( field.length() == 6 );
+    assertEquals( field, "Coyote" );
+
+    // Truncation Tests = = = = =
+    field = StringUtil.fixedLength( text, 5, LEFT, '*' );
+    assertTrue( field.length() == 5 );
+    assertEquals( field, "Coyot" );
+
+    field = StringUtil.fixedLength( text, 5, CENTER, '*' );
+    assertTrue( field.length() == 5 );
+    assertEquals( field, "Coyot" );
+
+    field = StringUtil.fixedLength( text, 5, RIGHT, '*' );
+    assertTrue( field.length() == 5 );
+    assertEquals( field, "oyote" );
+
+    field = StringUtil.fixedLength( text, 4, LEFT, '*' );
+    assertTrue( field.length() == 4 );
+    assertEquals( field, "Coyo" );
+
+    field = StringUtil.fixedLength( text, 4, CENTER, '*' );
+    assertTrue( field.length() == 4 );
+    assertEquals( field, "oyot" );
+
+    field = StringUtil.fixedLength( text, 4, RIGHT, '*' );
+    assertTrue( field.length() == 4 );
+    assertEquals( field, "yote" );
+  }
+
+
 }
